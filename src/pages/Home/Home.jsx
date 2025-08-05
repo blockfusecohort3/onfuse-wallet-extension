60182ae672e52ce9648722aecec27bfb7794df3e
abbracx <tankoraphael@gmail.com>
2025-08-05 09:03:29 +0100

    removed unused and deprecated files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaRegCircleDot } from "react-icons/fa6";
 import { BsSendFill } from "react-icons/bs";
